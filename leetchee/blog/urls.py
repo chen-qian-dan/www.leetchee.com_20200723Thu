5618d21b731c8bf77e5234e4108c136f858cc3b9
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-24 04:58:58 +0930

    Bugfix : update name from page-home to page_home

--- leetchee/blog/urls.py
+++ leetchee/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_home, name='blog-home'),
+    path('', views.blog_home, name='blog_home'),
 ]
--- leetchee/page/urls.py
+++ leetchee/page/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.page_home, name='page-home'),
-    path('about/', views.page_about, name='page-about'),
+    path('', views.page_home, name='page_home'),
+    path('about/', views.page_about, name='page_about'),
 ]
