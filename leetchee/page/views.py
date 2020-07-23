36573ab64e149aeef2926977e6edfb42c2233d12
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-24 03:34:32 +0930

    Create home & about route with templates.

--- leetchee/page/views.py
+++ leetchee/page/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+
 
 # Create your views here.
 
 def page_home(request):
-    return HttpResponse('<h1>page-home</h1>')
+    return render(request, 'page/page_home.html')
 
 
 def page_about(request):
-    return HttpResponse('<h1>page-about</h1>')
+    return render(request, 'page/page_about.html')
