75bb740e3ff6d7a4b8d11305ae434c5907c940fe
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-24 04:11:11 +0930

    Blog: add data : blogs.

--- leetchee/blog/views.py
+++ leetchee/blog/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+blogs = [
+    {
+        'author': 'Qian', 
+        'title': 'ML', 
+        'content': 'Machine Learning', 
+        'date_posted': 'July 24, 2020', 
+    }, 
+    {
+        'author': 'dan', 
+        'title': 'CV', 
+        'content': 'Computer Vision', 
+        'date_posted': 'July 24, 2020', 
+    },
+]
 def blog_home(request):
-    return render(request, 'blog/blog_home.html')
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/blog_home.html', context)
