43d4b091bf72cb43260383499907b310b46af041
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-24 09:59:40 +0930

    blog : link database to views.py

--- leetchee/blog/views.py
+++ leetchee/blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
-blogs = [
-    {
-        'author': 'Qian', 
-        'title': 'ML', 
-        'content': 'Machine Learning', 
-        'date_posted': 'July 24, 2020', 
-    }, 
-    {
-        'author': 'dan', 
-        'title': 'CV', 
-        'content': 'Computer Vision', 
-        'date_posted': 'July 24, 2020', 
-    },
-]
+# blogs = [
+#     {
+#         'author': 'Qian', 
+#         'title': 'ML', 
+#         'content': 'Machine Learning', 
+#         'date_posted': 'July 24, 2020', 
+#     }, 
+#     {
+#         'author': 'dan', 
+#         'title': 'CV', 
+#         'content': 'Computer Vision', 
+#         'date_posted': 'July 24, 2020', 
+#     },
+# ]
 def blog_home(request):
     context = {
         'title': '博客',
-        'blogs': blogs
+        'blogs': Blog.objects.all()
     }
     return render(request, 'blog/blog_home.html', context)
