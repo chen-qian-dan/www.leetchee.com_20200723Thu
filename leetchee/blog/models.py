91d17e244c032b51b6b8635425d639ba0f586cdc
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-07-24 09:40:00 +0930

    blog : create database.

--- leetchee/blog/models.py
+++ leetchee/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Blog(models.Model):
+    # on_delete=models.CASCADE means if the user is deleted, 
+    # we want to delete all the posts from that user as well. 
+    author      = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    title       = models.CharField(max_length=100)
+    content     = models.TextField()
+    image       = models.ImageField(upload_to = "blog_img/", blank=True)
+
+    # Make sql result more script
+    def __str__(self):
+        return self.title
